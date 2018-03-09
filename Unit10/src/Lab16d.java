84d98a95577e058442c45414d2c75aadf978e658
haj1990 <HaJ1990@sduhsd.lan>
2018-03-09 10:46:39 -0800

    Unit 10

---
+++ Unit10/src/Lab16d.java
@@ -0,0 +1,17 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Lab16d
+{
+	public static void main( String args[] )
+	{
+		//make a new MadLib
+		MadLib myMadLib = new MadLib("C:\\Users\\haj1990\\Desktop\\Unit10-2016\\Unit10-Assignments\\Lab16d\\story.dat");
+		out.println("\n");
+	}
+}
